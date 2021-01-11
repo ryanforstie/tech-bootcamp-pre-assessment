eadfc249e2d6f1b0b89b7794f6f9c7559a114c26
RForsti1 <ryan.forstie1@t-mobile.com>
2021-01-11 12:24:28 -0800

    Complete assessment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pre-assessment';
+  addTotal: number;
+  subTotal: number;
+  num1: number;
+  num2: number;
+  num3: number;
+  num4: number;
+
+  getNum1(val: string) {
+    this.num1 = parseInt(val);    
+  }
+
+  getNum2(val: string) {    
+    this.num2 = parseInt(val);
+  }
+
+  
+  getNum3(val: string) {
+    this.num3 = parseInt(val);    
+  }
+
+  getNum4(val: string) {    
+    this.num4 = parseInt(val);
+  }
+
+  add() {;
+    this.addTotal = this.num1 + this.num2;
+  }
+
+  subtract() {
+    this.subTotal = this.num3 - this.num4;
+  }
 }
